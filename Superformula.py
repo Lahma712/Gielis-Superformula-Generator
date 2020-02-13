7932aa37f687994a44fda83e9a3b40d315e62e7a
Lahr Max <52128659+Lahma712@users.noreply.github.com>
2020-02-13 14:57:32 +0100

    Update Superformula.py

--- Superformula.py
+++ Superformula.py
@@ -8,17 +8,15 @@ n1 = float(input("n1 = "))
 n2 = float(input("n2 = "))
 n3 = float(input("n3 = "))
 a = float(input("a = "))
-b= float(input("b = "))
+b = float(input("b = "))
 color = input("color (red/blue/yellow.../hexadecimal value): ")
 print("\nA .png file is saved to your desktop")
 
 th = np.arange(0,(2*np.pi), 0.001)
 with np.errstate(all='ignore'):
-    ro = abs(np.cos((m*th)/4)/a)**n2 + abs(np.sin((m*th)/4)/b)**n3
-    r = abs(ro) ** (-1/n1)
+    r = (abs(np.cos((m*th)/4)/a)**n2 + abs(np.sin((m*th)/4)/b)**n3) ** (-1/n1)
     y = r * np.sin(th)
     x = r * np.cos(th)
-
 
 ax = plot.subplot(111)
 ax.grid(False)
