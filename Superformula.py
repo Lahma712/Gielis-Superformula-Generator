c7b5dd41e4834ddb36144bd10d3c61f69eeb0d72
Lahr Max <52128659+Lahma712@users.noreply.github.com>
2020-02-13 15:38:17 +0100

    Update Superformula.py

--- Superformula.py
+++ Superformula.py
@@ -3,7 +3,8 @@ import numpy as np
 import getpass
 host = getpass.getuser()
 
-m = float(input("m (=m1=m2): "))
+m1 = float(input("m1: "))
+m2 = float(input("m2: "))
 n1 = float(input("n1 = "))
 n2 = float(input("n2 = "))
 n3 = float(input("n3 = "))
@@ -14,7 +15,7 @@ print("\nA .png file is saved to your desktop")
 
 th = np.arange(0,(2*np.pi), 0.001)
 with np.errstate(all='ignore'):
-    r = (abs(np.cos((m*th)/4)/a)**n2 + abs(np.sin((m*th)/4)/b)**n3) ** (-1/n1)
+    r = (abs(np.cos((m1*th)/4)/a)**n2 + abs(np.sin((m2*th)/4)/b)**n3) ** (-1/n1)
     y = r * np.sin(th)
     x = r * np.cos(th)
 
