48815574f64cc7e2324b168a295142ddc2747345
Lahr Max <52128659+Lahma712@users.noreply.github.com>
2020-02-13 15:51:39 +0100

    Update Superformula.py

--- Superformula.py
+++ Superformula.py
@@ -3,6 +3,7 @@ import numpy as np
 import getpass
 host = getpass.getuser()
 
+k = int(input("Number of rounds (k*pi): ")
 m = float(input("m: "))
 n1 = float(input("n1 = "))
 n2 = float(input("n2 = "))
@@ -12,7 +13,7 @@ b = float(input("b = "))
 color = input("color (red/blue/yellow.../hexadecimal value): ")
 print("\nA .png file is saved to your desktop")
 
-th = np.arange(0,(2*np.pi), 0.001)
+th = np.arange(0,(k*np.pi), 0.001)
 with np.errstate(all='ignore'):
     r = (abs(np.cos((m*th)/4)/a)**n2 + abs(np.sin((m*th)/4)/b)**n3) ** (-1/n1)
     y = r * np.sin(th)
