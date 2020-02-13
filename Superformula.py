c1ad159ad56db69e4cbf7a38872183fb2876bbf1
Lahr Max <52128659+Lahma712@users.noreply.github.com>
2020-02-13 19:03:52 +0100

    Update Superformula.py
    added m1 and m2 parameters

--- Superformula.py
+++ Superformula.py
@@ -3,8 +3,9 @@ import numpy as np
 import getpass
 host = getpass.getuser()
 
-k = int(input("Number of rounds (2*k*pi): "))
-m = eval(input("m: "))
+k = int(input("Number of rounds (k*pi): "))
+m1 = eval(input("m1: "))
+m2 = eval(input("m2: "))
 n1 = eval(input("n1 = "))
 n2 = eval(input("n2 = "))
 n3 = eval(input("n3 = "))
@@ -13,9 +14,9 @@ b = eval(input("b = "))
 color = input("color (red/blue/yellow.../hexadecimal value): ")
 print("\nA .png file is saved to your desktop")
 
-th = np.arange(0,(2*k*np.pi), 0.001) #angle theta
+th = np.arange(0,(k*np.pi), 0.0001)
 with np.errstate(all='ignore'):
-    r = (abs(np.cos((m*th)/4)/a)**n2 + abs(np.sin((m*th)/4)/b)**n3) ** (-1/n1)
+    r = (abs(np.cos((m1*th)/4)/a)**n2 + abs(np.sin((m2*th)/4)/b)**n3) ** (-1/n1)
     y = r * np.sin(th)
     x = r * np.cos(th)
 
@@ -24,4 +25,6 @@ ax.grid(False)
 ax.axis("off")
 ax.plot(x,y, color = color)
 plot.gca().set_aspect('equal')
+
 plot.savefig("c:\\Users\\{}\\Desktop\\superformula.png".format(host))
+plot.show()
