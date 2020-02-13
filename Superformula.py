1c3365882f0748bf1344c9fce6c3a338dfb90b9d
Lahr Max <52128659+Lahma712@users.noreply.github.com>
2020-02-13 20:09:04 +0100

    Update Superformula.py

--- Superformula.py
+++ Superformula.py
@@ -1,9 +1,10 @@
 import matplotlib.pyplot as plot
 import numpy as np
 import getpass
+from PIL import Image
 host = getpass.getuser()
 
-k = int(input("Number of rounds (2*k*pi): "))
+k = int(input("Number of rounds (k*pi): "))
 m1 = eval(input("m1: "))
 m2 = eval(input("m2: "))
 n1 = eval(input("n1 = "))
@@ -11,20 +12,23 @@ n2 = eval(input("n2 = "))
 n3 = eval(input("n3 = "))
 a = eval(input("a = "))
 b = eval(input("b = "))
-color = input("color (red/blue/yellow.../hexadecimal value): ")
+color = input("Line color (red/blue/yellow.../#hexadecimal value): ")
+colorfill = input("Fill color: ")
 print("\nA .png file is saved to your desktop")
 
-th = np.arange(0,(2*k*np.pi), 0.0001)
+theta = np.arange(0,(2*k*np.pi), 0.001)
 with np.errstate(all='ignore'):
-    r = (abs(np.cos((m1*th)/4)/a)**n2 + abs(np.sin((m2*th)/4)/b)**n3) ** (-1/n1)
-    y = r * np.sin(th)
-    x = r * np.cos(th)
+    r = (abs(np.cos((m1*theta)/4)/a)**n2 + abs(np.sin((m2*theta)/4)/b)**n3) ** (-1/n1)
+    y = r * np.sin(theta)
+    x = r * np.cos(theta)
 
-ax = plot.subplot(111)
-ax.grid(False)
-ax.axis("off")
-ax.plot(x,y, color = color)
+graph = plot.subplot(111)
+graph.grid(False)
+graph.axis("off")
+graph.plot(x,y, color = color)
+graph.fill(x,y, color = colorfill)
+
 plot.gca().set_aspect('equal')
 
 plot.savefig("c:\\Users\\{}\\Desktop\\superformula.png".format(host))
-plot.show()
+Image.open("c:\\Users\\{}\\Desktop\\superformula.png".format(host)).show()
